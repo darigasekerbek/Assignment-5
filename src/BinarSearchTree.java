bbb92e8c587d61264c605296fa1a733798fd82d1
Abylai <221714@astanait.edu.kz>
2023-05-21 19:15:35 +0200

    private Node putNode(Node node, K key, V value)

--- src/BinarSearchTree.java
+++ src/BinarSearchTree.java
@@ -1,3 +1,4 @@
+
 public class BinarSearchTree <K extends Comparable<K>, V> implements Iterable<BinarSearchTree.Entry<K, V>> {
     private Node root;
     private int size;
@@ -5,10 +6,24 @@ public class BinarSearchTree <K extends Comparable<K>, V> implements Iterable<Bi
         root = putNode(root, key, value);
     }
     private Node putNode(Node node, K key, V value){
+        //If current node is null, create a new node with given key and value
         if (node == null){
             size++;
             return new Node(key,value);
         }
+        //Compare given key with key of current node
+        int comparison = key.compareTo(node.key);
+        if (comparison > 0){
+            node.right = putNode(node.right, key, value);
+        }
+        else if (comparison < 0){
+            node.left = putNode(node.left, key, value);
+        }
+        else {
+            node.value = value;
+        }
+        //return modified node
+        return node;
     }
     //Node class represent a node in a binary tree ds.
     //It stores a key, value pair and references to its left and right child nodes
