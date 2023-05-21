84388b9bf1c7b49e3eebfd06f761cb90a632d785
Abylai <221714@astanait.edu.kz>
2023-05-21 19:23:16 +0200

    public V get(K key) private Node getNode(Node node, K key)

--- src/BinarSearchTree.java
+++ src/BinarSearchTree.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 
 public class BinarSearchTree <K extends Comparable<K>, V> implements Iterable<BinarSearchTree.Entry<K, V>> {
     private Node root;
@@ -25,6 +26,33 @@ public class BinarSearchTree <K extends Comparable<K>, V> implements Iterable<Bi
         //return modified node
         return node;
     }
+    public V get(K key){
+        Node node = getNode(root, key);
+        return node != null ? node.value : null;
+    }
+    //This method search for a node with given key in tree
+    //returns the node if found, or null if the key isn't present
+    private Node getNode(Node node, K key){
+        while (node != null){
+            int comparison = key.compareTo(node.key);
+            if(comparison > 0){
+                node = node. right;
+            }
+            else if (comparison < 0){
+                node = node.left;
+            }
+            else {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Iterator<BinarSearchTree.Entry<K, V>> iterator() {
+        return null;
+    }
+
     //Node class represent a node in a binary tree ds.
     //It stores a key, value pair and references to its left and right child nodes
     private class Node{
